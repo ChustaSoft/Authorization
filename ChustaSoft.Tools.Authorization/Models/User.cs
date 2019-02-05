e632edc41aad981811856011652ab127f7d844f2
Xavi Rueda Abelaira <xavi_rueda@outlook.com>
2019-02-05 18:29:15 +0100

    User model exposing main Identity properties

--- ChustaSoft.Tools.Authorization/Models/User.cs
+++ ChustaSoft.Tools.Authorization/Models/User.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 
+
 namespace ChustaSoft.Tools.Authorization.Models
 {
     public class User : IdentityUser<Guid>
     {
         public string Culture { get; set; }
+
+        public override Guid Id { get; set; }
+
+        public override string UserName { get; set; }
+
+        public override string Email { get; set; }
+
+        public override string PhoneNumber { get; set; }
     }
 }
