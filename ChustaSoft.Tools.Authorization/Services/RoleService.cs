fbd2f8ab0456c809da074cf89981341079465f44
Xavi Rueda Abelaira <xavi_rueda@outlook.com>
2019-02-11 20:50:26 +0100

    - Updating NuGet references - Creating Beta IRoleService

---
+++ ChustaSoft.Tools.Authorization/Services/RoleService.cs
@@ -0,0 +1,41 @@
+﻿using ChustaSoft.Tools.Authorization.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+
+
+namespace ChustaSoft.Tools.Authorization.Services
+{
+    public class RoleService : IRoleService
+    {
+
+        #region Fields
+
+        private readonly RoleManager<Role> _roleManager;
+
+        #endregion
+
+
+        #region Constructor
+
+        public RoleService(RoleManager<Role> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        #endregion
+
+
+        #region Public methods
+
+        public Task<Role> Get(Guid roleId)
+        {
+            var role = _roleManager.FindByIdAsync(roleId.ToString());
+
+            return role;
+        }
+
+        #endregion
+
+    }
+}
