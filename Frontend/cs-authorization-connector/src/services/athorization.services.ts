4c0bce83bad63a16d0a602a37bc444f44fe2f50a
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-10-31 00:27:40 +0100

    Fixed AuthorizationService register Promise

--- Frontend/cs-authorization-connector/src/services/athorization.services.ts
+++ Frontend/cs-authorization-connector/src/services/athorization.services.ts
@@ -1,21 +1,25 @@
 import { Credentials, Session } from "..";
 import { ActionResponse } from '@chustasoft/cs-common'
 
-
 export class AuthorizationService {
 
     public async register(credentials: Credentials): Promise<Session> {
-        return new Promise<ActionResponse<Session>>(async (resolve, reject) => {
-            return await fetch('https://localhost:44374/api/auth/register', {
-                method: 'POST',
-                body: JSON.stringify(credentials),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': `application/json, text/plain, */*`
+        return fetch('https://localhost:44374/api/auth/register', {
+            method: 'POST',
+            body: JSON.stringify(credentials),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': `application/json, text/plain, */*`
+            }})
+            .then(httpRespose => {
+                if (!httpRespose.ok) {
+                    throw new Error(httpRespose.statusText)
                 }
+                return httpRespose.json() as Promise<ActionResponse<Session>>;
+            })
+            .then(actionResponse => {
+                return actionResponse.data
             });
-        })
-            .then(x => x.data);
     }
 
 }
