1d67a2e388d9b3ff97d478370dd4a6a620c2b480
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-11-01 18:55:09 +0100

    Added login functionality to AuthorizationService via Common HtpService

--- Frontend/cs-authorization-connector/src/services/athorization.services.ts
+++ Frontend/cs-authorization-connector/src/services/athorization.services.ts
@@ -1,33 +1,27 @@
 import { Credentials, Session } from "..";
-import { ActionResponse } from '@chustasoft/cs-common'
+import { HttpService } from '@chustasoft/cs-common'
 
 export class AuthorizationService {
 
+    private httpService: HttpService;
     private authorizationApiUrl: string;
 
     
     constructor(authorizationApiUrl: string) {
         this.authorizationApiUrl = authorizationApiUrl;
+
+        this.httpService = new HttpService();
     }
 
 
     public async register(credentials: Credentials): Promise<Session> {
-        return fetch(`${this.authorizationApiUrl}api/auth/register`, {
-            method: 'POST',
-            body: JSON.stringify(credentials),
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': `application/json, text/plain, */*`
-            }})
-            .then(httpRespose => {
-                if (!httpRespose.ok) {
-                    throw new Error(httpRespose.statusText)
-                }
-                return httpRespose.json() as Promise<ActionResponse<Session>>;
-            })
-            .then(actionResponse => {
-                return actionResponse.data
-            });
+        return this.httpService.post<Credentials, Session>(
+            `${this.authorizationApiUrl}api/auth/register`, credentials);
+    }
+
+    public async login(credentials: Credentials): Promise<Session> {
+        return this.httpService.post<Credentials, Session>(
+            `${this.authorizationApiUrl}api/auth/login`, credentials);
     }
 
 }
