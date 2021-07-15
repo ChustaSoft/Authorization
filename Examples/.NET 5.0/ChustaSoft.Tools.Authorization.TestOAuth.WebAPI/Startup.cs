2317fc7e73307ccfaa484042f8e2fe1faa7157f9
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-07-16 00:41:51 +0200

    Test and example implementation for OAuth

---
+++ Examples/.NET 5.0/ChustaSoft.Tools.Authorization.TestOAuth.WebAPI/Startup.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ChustaSoft.Tools.Authorization.TestOAuth.WebAPI
+{
+    public class Startup
+    {
+        public readonly IConfiguration _configuration;
+
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.WithOAuthProvider();
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOAuthProvider();
+        }
+    }
+}
