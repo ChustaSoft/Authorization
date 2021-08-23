3b0bcd2d805f393deb5d17721998925c18e4d387
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2021-08-23 10:38:08 +0200

    Removed unnecessary using

--- NuGet/ChustaSoft.Tools.Authorization.SqlServer/Configuration/ConfigurationHelper.cs
+++ NuGet/ChustaSoft.Tools.Authorization.SqlServer/Configuration/ConfigurationHelper.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Reflection;
 
 namespace ChustaSoft.Tools.Authorization
